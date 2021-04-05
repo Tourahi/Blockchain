55dd4d159444a0f63e07c8861646be1982cf686a
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-05 16:07:56 +0100

    Refactor :: Conforming to Betty Style

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -5,14 +5,14 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 {
   SHA256_CTX c;
 
-  if(digest == NULL)
+  if (digest == NULL)
   {
     return (NULL);
   }
 
   SHA256_Init(&c);
   SHA256_Update(&c, s, len);
-  SHA256_Final(digest ,&c);
+  SHA256_Final(digest, &c);
 
   return (digest);
 }
