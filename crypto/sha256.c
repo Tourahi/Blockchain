50f27068dff2da6cc0ce8ebd713c08e5e63b9eb9
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-05 16:03:07 +0100

    Digest should not be null in sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -3,15 +3,17 @@
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-  if(!digest)
-    return NULL;
-
   SHA256_CTX c;
+
+  if(digest == NULL)
+  {
+    return (NULL);
+  }
 
   SHA256_Init(&c);
   SHA256_Update(&c, s, len);
   SHA256_Final(digest ,&c);
 
-  return digest;
+  return (digest);
 }
 
