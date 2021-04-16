796cc80092a2dcf02d111553fd422d5a8cddfbc3
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 11:14:29 +0000

    Commenting the functions

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,18 +1,23 @@
 #include "hblk_crypto.h"
 
 
+/**
+ * ec_create - creates a new EC key pair
+ *
+ * Return: A pointer to an EC_KEY structure
+ */
 EC_KEY *ec_create(void)
 {
-  EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
+	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
 
-  if (!(key))
-    return (NULL);
+	if (!(key))
+		return (NULL);
 
-  if (!EC_KEY_generate_key(key))
-  {
-    EC_KEY_free(key);
-    return (NULL);
-  }
-  return (key);
+	if (!EC_KEY_generate_key(key))
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	return (key);
 }
 
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,19 +1,26 @@
 
 #include "hblk_crypto.h"
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+/**
+ * sha256 - computes the hash of a sequence of bytes
+ * @s : the sequence of bytes to be hashed
+ * @len : the number of bytes to hash in s
+ * @d : the stored hash
+ *
+ * Return: A pointer to digest
+ */
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t d[SHA256_DIGEST_LENGTH])
 {
-  SHA256_CTX c;
+	SHA256_CTX c;
 
-  if (digest == NULL)
-  {
-    return (NULL);
-  }
+	if (d == NULL)
+	{
+		return (NULL);
+	}
 
-  SHA256_Init(&c);
-  SHA256_Update(&c, s, len);
-  SHA256_Final(digest, &c);
+	SHA256_Init(&c);
+	SHA256_Update(&c, s, len);
+	SHA256_Final(d, &c);
 
-  return (digest);
+	return (digest);
 }
-
