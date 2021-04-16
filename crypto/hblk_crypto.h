b37d73c91a6096d369da240f4461e70f162a4619
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 11:33:08 +0000

    Refactorign the header

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -15,13 +15,19 @@
 #define PRI_FILENAME "key.pem"
 #define PUB_FILENAME "key_pub.pem"
 
+/**
+ * struct sig_s - EC Signature structure
+ *
+ * @sig: Signature buffer. The whole space may not be used
+ * @len: Actual signature size.
+ */
 typedef struct sig_s
 {
-  uint8_t sig[SIG_MAX_LEN];
-  uint8_t len;
+	uint8_t sig[SIG_MAX_LEN];
+	uint8_t len;
 } sig_t;
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t di[SHA256_DIGEST_LENGTH]);
 
 EC_KEY *ec_create(void);
 
