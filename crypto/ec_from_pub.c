3bca2f28b3143fa304a39fb2940d9e6bf5ea7e92
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 13:03:10 +0000

    function max length

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -6,37 +6,25 @@
  *
  * Return: The generated EC_KEY struct
  */
-
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key;
 	EC_POINT *point;
-	EC_GROUP *group;
 	int isConverted = 0;
 	int isPublicKeySet = 0;
 
 	if (!pub)
 		return (NULL);
 
-	/*
-	*	See hblk_crypto.h fo the def of the EC_CURVE nid
-	*/
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (NULL);
 
-	group = EC_KEY_get0_group(key);
-	if (!group)
-		return (NULL);
-	/*
-	* Create a new point on the curve
-	* See : https://www.openssl.org/docs/man1.1.0/man3/EC_POINT_new.html
-	*/
-	point = EC_POINT_new(group);
+	point = EC_POINT_new(EC_KEY_get0_group(key));
 	if (!point)
 		return (NULL);
 
-	isConverted = EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL);
+	isConverted = EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL);
 	isPublicKeySet = EC_KEY_set_public_key(key, point);
 
 	if (!isConverted || !isPublicKeySet)
