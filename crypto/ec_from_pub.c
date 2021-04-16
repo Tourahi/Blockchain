fdc235a4a8dcbd8f51fffb941a35baa04724e575
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 12:53:51 +0000

    EC_KEY from public key commenting the function

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -6,7 +6,6 @@
  *
  * Return: The generated EC_KEY struct
  */
-
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key;
@@ -16,27 +15,16 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	if (!pub)
 		return (NULL);
 
-	/*
-	*	See hblk_crypto.h fo the def of the EC_CURVE nid
-	*/
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (NULL);
 
 	group = EC_KEY_get0_group(key);
-	if (!group)
-		return (NULL);
-	/*
-	* Create a new point on the curve
-	* See : https://www.openssl.org/docs/man1.1.0/man3/EC_POINT_new.html
-	*/
+
 	point = EC_POINT_new(group);
 	if (!point)
 		return (NULL);
 
-	/*
-	*	convert from octet to EC_POINT
-	*/
 	int isConverted = EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL);
 	int isPublicKeySet = EC_KEY_set_public_key(key, point);
 
