77324269963d49194510478a0f486e7a6227b2fe
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 18:54:31 +0000

    ec_verify && test main

---
+++ crypto/ec_verify.c
@@ -0,0 +1,29 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_verify - verifies a message
+ * @key: pointer to struct containing the key pair
+ * @msg: the message
+ * @msglen: length of msg
+ * @sig: address to store signature
+ *
+ * Return: 1 | 0
+ */
+
+int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen,
+	sig_t const* sig)
+{
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+
+	if ( !msg || !key || !sig)
+		return (0);
+	if (!EC_KEY_check_key(key))
+		return (0);
+	if (!SHA256(msg, msglen, hash))
+		return (0);
+	if (!ECDSA_verify(0, hash, SHA256_DIGEST_LENGTH, sig->sig, sig->len,
+			  (EC_KEY *)key))
+		return (0);
+
+	return (1);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -52,8 +52,11 @@ EC_KEY *ec_load(char const *folder);
 /*
 *	 signs a given set of bytes, using a given EC_KEY private key
 */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
-	size_t msglen, sig_t *sig);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg
+	, size_t msglen, sig_t *sig);
+
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen
+	, sig_t const *sig);
 
 #endif
 
