0cd5c978a3c32b5b4651867af84336e32dc65236
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 18:13:33 +0000

    Fix Bug in ec_sign

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -13,18 +13,24 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg
 	, size_t msglen, sig_t *sig)
 {
-	unsigned int len;
+	unsigned char md[SHA256_DIGEST_LENGTH];
 
 	if (!key || !msg || !sig)
 		return (NULL);
 
-	len = sig->len;
-
-	if (ECDSA_sign(0, msg, msglen, sig->sig, &len,
-		(EC_KEY *)key) != 1)
+	if (!EC_KEY_check_key(key))
 		return (NULL);
 
-	sig->len = len;
+	if (!SHA256(msg, msglen, md))
+		return (NULL);
+
+	sig->len = ECDSA_size(key);
+	if (!sig->len)
+		return (NULL);
+
+	if (!ECDSA_sign(EC_CURVE, md, SHA256_DIGEST_LENGTH, sig->sig,
+				(unsigned int *)&(sig->len), (EC_KEY *)key))
+		return (NULL);
 
 	return (sig->sig);
 }
