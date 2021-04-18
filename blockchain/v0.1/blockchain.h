ec0405b244b298bf6d8f3a730cc522446303ab6e
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-18 17:57:24 +0000

    add block_destroy Prototype in the header file

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -115,7 +115,7 @@ blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev,
 					int8_t const *data, uint32_t data_len);
 
-void block_destroy(block_t *block)
+void block_destroy(block_t *block);
 
 
 #endif
