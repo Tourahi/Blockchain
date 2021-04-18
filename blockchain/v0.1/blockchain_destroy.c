fbb6e0356ec90bf9fe306da8ee44f2a76d4265aa
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-18 18:05:43 +0000

    Commenting the blockchain_destroy function

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,6 +1,9 @@
 #include "blockchain.h"
 
-
+/**
+ * blockchain_destroy - deletes an existing Blockchain
+ * @blockchain: points to the Blockchain structure to delete
+ */
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
