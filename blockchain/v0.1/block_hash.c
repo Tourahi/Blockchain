1e0732b3a7d252139d1cd0ad96b03a9a63fb6b6a
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-18 18:25:01 +0000

    Commenting Hash block

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,6 +1,13 @@
 #include "blockchain.h"
 
 
+/**
+ * block_hash - computes the hash of a Block
+ * @block: points to the Block to be hashed
+ * @hash_buf: The resulting hash must be stored in it
+ *
+ * Return: pointer to hash_buf
+ */
 
 uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
