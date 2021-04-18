335730cd81d53656a2b09fd9115d754fad66d756
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-18 18:26:22 +0000

    Fix :: block_hash.c:14: WARNING: Statements terminations use 1 semicolon

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -8,11 +8,10 @@
  *
  * Return: pointer to hash_buf
  */
-
 uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len = sizeof(block->info) + block->data.len;;
+	size_t len = sizeof(block->info) + block->data.len;
 
 	if (!block || !hash_buf)
 		return (NULL);
