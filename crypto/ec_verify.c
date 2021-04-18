080417e29ddde4baa090f1518b605cbe7ba7d4df
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-18 21:25:25 +0000

    refactor ec_verify

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -13,7 +13,6 @@
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	sig_t const *sig)
 {
-	int res = 0;
 
 	if (!msg || !key || !sig)
 		return (0);
@@ -21,7 +20,10 @@ int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	if (!EC_KEY_check_key(key))
 		return (0);
 
-	res = ECDSA_verify(0, (unsigned char *)msg, msglen, sig->sig, sig->len,
-				(EC_KEY *)key);
-	return (res);
+	if (ECDSA_verify(0, msg, msglen, sig->sig, (int)sig->len, (EC_KEY *)key) == 1)
+	{
+		return (1);
+	}
+
+	return (0);
 }
