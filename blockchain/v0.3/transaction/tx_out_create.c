2e55feeb7f3c265a65dbf4c4f6185a7788b2a4b2
maromaroXD <amine-fb-fb@hotmail.fr>
2021-05-09 20:30:14 +0000

    Finalizing

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -2,21 +2,19 @@
 
 /**
  * tx_out_create - creates a new transaction output structure
- * @amount: is the amount of the transaction
- * @pub: is the public key of the transaction receiver
- * Return: ptr | NULL
+ * @amount: the transaction amount
+ * @pub: public key of receiver
+ * Return: pointer to new structure or NULL
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-	tx_out_t *transaction = calloc(1, sizeof(*transaction));
+	tx_out_t *t = calloc(1, sizeof(*t));
 
-	if (!transaction)
+	if (!t)
 		return (NULL);
-	transaction->amount = amount;
-	memcpy(transaction->pub, pub, sizeof(transaction->pub));
-	if (!sha256((int8_t const *)transaction,
-			sizeof(transaction->amount) + sizeof(transaction->pub),
-			transaction->hash))
-		return (free(transaction), NULL);
-	return (transaction);
+	t->amount = amount;
+	memcpy(t->pub, pub, sizeof(t->pub));
+	if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
+		return (free(t), NULL);
+	return (t);
 }
